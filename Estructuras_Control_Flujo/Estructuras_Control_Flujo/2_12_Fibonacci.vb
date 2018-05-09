104cc6e90f0a978a18af99ec1e47c1c96fff800f
Darío González <dario-goirad@hotmail.com>
2018-05-09 17:34:26 -0300

    Solucion Estructuras_Control_Flujo

--- Estructuras_Control_Flujo/Estructuras_Control_Flujo/2_12_Fibonacci.vb
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/2_12_Fibonacci.vb
@@ -1,3 +1,16 @@
 ﻿Module Fibonacci
+    Dim cant As Byte = 0
+    Dim a As Integer = 0
+    Dim b As Integer = 1
 
+    Sub Main()
+        Console.WriteLine("Secuencia Fibonacci---------")
+        Do
+            Console.WriteLine(a)
+            Console.WriteLine(b)
+            cant += 1
+            a = a + b
+            b = b + a
+        Loop While cant <= 20
+    End Sub
 End Module
